083ae61536344186f208c497fef866180a8690ef
Adithya Prasetio Hutahaean <145269520+HikaruYo@users.noreply.github.com>
2024-05-29 07:51:44 +0700

    add style

--- dashboard.php
+++ dashboard.php
@@ -14,37 +14,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>  
-      
-    <p>Anda login sebagai: <?php echo htmlspecialchars($_SESSION['role']); ?></p>
-    
-    <?php
-        // if ($_SESSION['role']=='superadmin') {
-        //     echo "<h1>Selamat Datang Tuan, " . $_SESSION['username'] . "</h1>";
-        // } elseif ($_SESSION['role']=='admin') {
-        //     echo "<h1>Selamat Datang, " . $_SESSION['username'] . "</h1>";
-        // } elseif ($_SESSION['role']=='regular') {
-        //     echo "<h1>Hai, " . $_SESSION['username'] . "</h1>";
-        // }
+    <div class="section">
+        <p>Anda login sebagai: <?php echo htmlspecialchars($_SESSION['role']); ?></p>
+        
+        <?php
+            // if ($_SESSION['role']=='superadmin') {
+            //     echo "<h1>Selamat Datang Tuan, " . $_SESSION['username'] . "</h1>";
+            // } elseif ($_SESSION['role']=='admin') {
+            //     echo "<h1>Selamat Datang, " . $_SESSION['username'] . "</h1>";
+            // } elseif ($_SESSION['role']=='regular') {
+            //     echo "<h1>Hai, " . $_SESSION['username'] . "</h1>";
+            // }
 
-        //Perbaikan code menggunakan array
-                    
-        // Pesan selamat datang sesuai role
-        $pesan = [
-            'superadmin'=> 'Selamat Datang Tuan',
-            'admin'=> 'Selamat Datang',
-            'regular'=> 'Hai'
-        ];
+            //Perbaikan code menggunakan array
+                        
+            // Pesan selamat datang sesuai role
+            $pesan = [
+                'superadmin'=> 'Selamat Datang Tuan',
+                'admin'=> 'Selamat Datang',
+                'regular'=> 'Hai'
+            ];
 
-        $role = $_SESSION['role'];
-        $username = htmlspecialchars($_SESSION['username']);
-        echo "<h1>{$pesan[$role]}, {$username}</h1>" 
+            $role = $_SESSION['role'];
+            $username = htmlspecialchars($_SESSION['username']);
+            echo "<h1>{$pesan[$role]}, {$username}</h1>" 
 
-    ?>
+        ?>
 
-    <!-- Logout Function -->
-    <a href="logout.php">LogOut</a>
-
+        <!-- Logout Function -->
+        <a href="logout.php">LogOut</a>
+    </div>
 </body>
 </html>
